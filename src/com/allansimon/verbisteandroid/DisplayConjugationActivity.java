f003d04182ff36ed8e3d8ff6cb75521efbc4ce48
Allan Simon <simona@gobeta.com.cn>
2014-12-30 02:20:38 +0800

    added an activity (blank for the moment) to display latter conjugation

---
+++ src/com/allansimon/verbisteandroid/DisplayConjugationActivity.java
@@ -0,0 +1,16 @@
+package com.allansimon.verbisteandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayConjugationActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conjugation);
+    }
+
+
+
+}
--- src/com/allansimon/verbisteandroid/MainActivity.java
+++ src/com/allansimon/verbisteandroid/MainActivity.java
@@ -1,15 +1,30 @@
 package com.allansimon.verbisteandroid;
 
+import com.allansimon.verbisteandroid.DisplayConjugationActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
+    public final static String EXTRA_VERB = "com.allansimon.verbisteandroid.MESSAGE";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void conjugate(View view)
+    {
+        Intent intent = new Intent(this, DisplayConjugationActivity.class);
+        EditText enterVerb = (EditText) findViewById(R.id.enter_verb);
+        String verb = enterVerb.getText().toString();
+        intent.putExtra(EXTRA_VERB, verb);
+        startActivity(intent);
     }
 }
